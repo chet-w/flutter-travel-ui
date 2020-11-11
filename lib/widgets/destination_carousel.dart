cbedd338b94d3ebaf82faf5794a57088d278219a
Chet Wijesekera <chethana96@gmail.com>
2020-11-11 21:01:27 +1300

    Create destination cards, unstyled

--- lib/widgets/destination_carousel.dart
+++ lib/widgets/destination_carousel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_travel_ui_starter/models/destination_model.dart';
 
 class DestinationCarousel extends StatelessWidget {
   @override
@@ -21,6 +22,55 @@ class DestinationCarousel extends StatelessWidget {
                   fontWeight: FontWeight.w600,
                   letterSpacing: 1.0))
         ]),
+      ),
+      Container(
+        height: 300.0,
+        color: Colors.blue,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: destinations.length,
+          itemBuilder: (BuildContext context, int index) {
+            Destination destination = destinations[index];
+            return Container(
+              margin: EdgeInsets.all(10.0),
+              width: 210.0,
+              child: Stack(
+                children: [
+                  Container(
+                    height: 120.0,
+                    width: 200.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.white,
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            '${destination.activities.length} activities',
+                            style: TextStyle(
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.w600,
+                            )
+                          ),
+                          Text(
+                            '${destination.description}',
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            );
+          },
+        )
       )
     ]);
   }
