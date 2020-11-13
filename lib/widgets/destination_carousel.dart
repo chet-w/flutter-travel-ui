4542dbbab70f107177538ad52b3be1a48113d163
Chet Wijesekera <chethana96@gmail.com>
2020-11-13 22:54:09 +1300

    Add location names to images

--- lib/widgets/destination_carousel.dart
+++ lib/widgets/destination_carousel.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_travel_ui_starter/models/destination_model.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class DestinationCarousel extends StatelessWidget {
   @override
@@ -24,54 +25,110 @@ class DestinationCarousel extends StatelessWidget {
         ]),
       ),
       Container(
-        height: 300.0,
-        color: Colors.blue,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: destinations.length,
-          itemBuilder: (BuildContext context, int index) {
-            Destination destination = destinations[index];
-            return Container(
-              margin: EdgeInsets.all(10.0),
-              width: 210.0,
-              child: Stack(
-                children: [
-                  Container(
-                    height: 120.0,
-                    width: 200.0,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      color: Colors.white,
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text(
-                            '${destination.activities.length} activities',
-                            style: TextStyle(
-                              fontSize: 22.0,
-                              fontWeight: FontWeight.w600,
-                            )
+          height: 300.0,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: destinations.length,
+            itemBuilder: (BuildContext context, int index) {
+              Destination destination = destinations[index];
+              return Container(
+                  margin: EdgeInsets.all(10.0),
+                  width: 210.0,
+                  child: Stack(
+                    alignment: Alignment.topCenter,
+                    children: [
+                      Positioned(
+                        bottom: 5.0,
+                        child: Container(
+                          height: 120.0,
+                          width: 200.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: Colors.white,
                           ),
-                          Text(
-                            '${destination.description}',
-                            style: TextStyle(
-                              color: Colors.grey,
+                          child: Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Text(
+                                    '${destination.activities.length} activities',
+                                    style: TextStyle(
+                                      fontSize: 22.0,
+                                      fontWeight: FontWeight.w600,
+                                    )),
+                                Text(
+                                  '${destination.description}',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                        ],
+                        ),
                       ),
-                    ),
-                  ),
-                ],
-              )
-            );
-          },
-        )
-      )
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black26,
+                                offset: Offset(0.0, 2.0),
+                                blurRadius: 6.0,
+                              ),
+                            ]),
+                        // child: Text("hehe", style: TextStyle(color: Colors.black))
+                        child: Stack(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(20.0),
+                              child: Image(
+                                height: 180.0,
+                                width: 180.0,
+                                image: AssetImage(destination.imageUrl),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Positioned(
+                              left: 10.0,
+                              bottom: 10.0,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    destination.city,
+                                    style: TextStyle(
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  Row(
+                                    children: [
+                                      Icon(FontAwesomeIcons.locationArrow,
+                                          size: 10.0, color: Colors.white),
+                                      SizedBox(width: 5.0),
+                                      Text(
+                                        destination.country,
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ));
+            },
+          ))
     ]);
   }
 }
